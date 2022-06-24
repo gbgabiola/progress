86307e4d30cb3cc58253f170da667b34d5ddb240
Genesis Gabiola <gbgabiola@gmail.com>
2022-06-24 14:32:14 +0800

    Add form component inputs

---
+++ courses/udemy/react-the-complete-guide-incl-redux/04-react-state-events/02-form-inputs/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,27 @@
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  return (
+    <form action="">
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="">Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="">Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
