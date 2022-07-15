0e4fdb9eeda7cf8ca28fdd211e5b6ca0de63d089
Genesis Gabiola <gbgabiola@gmail.com>
2022-07-15 15:20:40 +0800

    Add debugging react apps module

---
+++ courses/udemy/react-the-complete-guide-incl-redux/07-debugging-react-apps/01-starter/src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -0,0 +1,20 @@
+import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
+import './CourseGoalList.css';
+
+const CourseGoalList = props => {
+  return (
+    <ul className="goal-list">
+      {props.items.map(goal => (
+        <CourseGoalItem
+          key={goal.id}
+          id={goal.id}
+          onDelete={props.onDeleteItem}
+        >
+          {goal.text}
+        </CourseGoalItem>
+      ))}
+    </ul>
+  );
+};
+
+export default CourseGoalList;
---
+++ courses/udemy/react-the-complete-guide-incl-redux/07-debugging-react-apps/02-final/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -0,0 +1,15 @@
+import './CourseGoalItem.css';
+
+const CourseGoalItem = props => {
+  const deleteHandler = () => {
+    props.onDelete(props.id);
+  };
+
+  return (
+    <li className="goal-item" onClick={deleteHandler}>
+      {props.children}
+    </li>
+  );
+};
+
+export default CourseGoalItem;
